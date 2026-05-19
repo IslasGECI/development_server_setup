1f4fdb0c9a181382275f1400bddfeb9ab5fe2233
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2026-05-19 11:56:26 -0700

    🦝 Chase the raccoon: upgrade to Ubuntu 26.04 LTS
    Ubuntu 26.04 LTS Resolute Raccoon landed on April 23, 2026 and is now
    available in the Azure Marketplace. Upgrade the provisioning container and
    the Azure VM image reference so new development servers start life on the
    latest LTS with its updated kernel, Rust toolchain, and five more years of
    upstream support.

--- src/devserver.tf
+++ src/devserver.tf
@@ -61,7 +61,7 @@ resource "azurerm_linux_virtual_machine" "devserver" {
 
   source_image_reference {
     publisher = "canonical"
-    offer     = "ubuntu-24_04-lts"
+    offer     = "ubuntu-26_04-lts"
     sku       = "server"
     version   = "latest"
   }
